4b5ebb7ab07746ade2c4f1bbe3dbb71722f152be
unstack-underflow <aronlomn@gmail.com>
2023-12-10 14:26:35 -0500

    initial commit | empty spring boot and vite react project

---
+++ src/main/java/com/aronlomner/minimalist_crm/MinimalistCrmApplication.java
@@ -0,0 +1,13 @@
+package com.aronlomner.minimalist_crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimalistCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinimalistCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aronlomner/minimalist_crm/MinimalistCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aronlomner.minimalist_crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinimalistCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
